fe3c617bff5754a418425e7efcef3716b0e8d76b
Matthew S. MacLennan <msmaclennan@gmail.com>
2015-04-17 16:21:24 -0700

    Create elsevier.url.R

---
+++ elsevier.url.R
@@ -0,0 +1,3 @@
+#
+#&httpAccept=%20application%2Fatom%2Bxml can be appended to the URL to ensure that xml is returned. When using R, this is
+#imperative to ensure that the package XML can be used to handle the output.
