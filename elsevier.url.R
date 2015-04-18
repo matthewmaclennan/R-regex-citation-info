45a140f89fd3e414c62bf6b0065b89ecc0b92e0d
Matthew S. MacLennan <msmaclennan@gmail.com>
2015-04-18 11:14:59 -0700

    Update elsevier.url.R

--- elsevier.url.R
+++ elsevier.url.R
@@ -1,3 +1,5 @@
 #
 #&httpAccept=%20application%2Fatom%2Bxml can be appended to the URL to ensure that xml is returned. When using R, this is
 #imperative to ensure that the package XML can be used to handle the output.
+cefasd.xml<-xmlTreeParse(getURL(
+"api.elsevier.com/content/search/scidir?apiKey=&query=capillary+electrophoresis+fatty+acid&httpAccept=%20application%2Fatom%2Bxml"))
