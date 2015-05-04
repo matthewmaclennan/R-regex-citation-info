f46f139c84efb3509b668302866ae93eb45e92fc
Matthew S. MacLennan <msmaclennan@gmail.com>
2015-05-04 00:20:46 -0700

    Update GoogleScholarLinks.R

--- GoogleScholarLinks.R
+++ GoogleScholarLinks.R
@@ -3,3 +3,5 @@
 #in order to bypass the https.
 getURL("https://scholar.google.ca/scholar?hl=en&q=%22capillary+electrophoresis%22+acid+%28fatty+OR+*oic+OR+naphthenic%29&btnG=&as_sdt=1%2C5&as_sdtp=",
 ssl.verifyhost=F,ssl.verifypeer=F,followlocation=T)
+#extract author and journal/repository information, where gs_a likely stands for "Google Scholar author"
+strsplit(unlist(xpathApply(htmlTreeParse(gsc1,useInternalNode=T),"//*[@class='gs_a']",xmlValue))," - ")
