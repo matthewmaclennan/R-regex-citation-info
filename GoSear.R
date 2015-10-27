9e79b9c44569d2112c432f3c90995294643ddce1
Matthew S. MacLennan <msmaclennan@gmail.com>
2015-10-26 18:06:01 -0700

    Added looping function for GoSear results

--- GoSear.R
+++ GoSear.R
@@ -3,3 +3,22 @@
 mirex.gr<-htmlTreeParse(getURL("https://www.google.ca/search?q=mirex",
   ssl.verifyhost=F,ssl.verifypeer=F,followlocation=T),useInternalNode=T)
 xpathApply(mirex.gr,"//div[@id='resultStats']")
+
+#Take a list of words from Word or other vector and convert to vector in R
+listo<-c("
+#Paste here the list from other program
+#Then type the following:
+")
+listo<-gsub(" ","+",unlist(strsplit(listo,"\n")))
+
+#loop it!
+GoSear1<-function(LIST){
+LISTY<-list()
+for(i in 1:length(LIST)){
+  LISTY[[i]]<-xpathApply(htmlTreeParse(getURL(paste0("https://www.google.ca/search?q=",LIST[i]),
+    ssl.verifyhost=F,ssl.verifypeer=F,followlocation=T),useInternalNode=T),"//div[@id='resultStats']")
+  }
+  LISTY
+}
+
+GoSearYear<-
